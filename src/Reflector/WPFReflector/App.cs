7b56f1ef242992fc887f177e3acda96f2c7c471d
홍창 장 <wiseking2@hanmail.net>
2024-02-19 13:48:46 +0900

    First Commit

---
+++ src/Reflector/WPFReflector/App.cs
@@ -0,0 +1,18 @@
+﻿using Jamesnet.Wpf.Controls;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFReflector
+{
+    internal class App : JamesApplication
+    {
+        protected override Window CreateShell()
+        {
+            Window window = new Window();
+            window.Title = "Heimdall Reflector";
+            window.Content = new Slider();
+
+            return window;
+        }
+    }
+}
---
+++ src/Reflector/WPFReflector/Stater.cs
@@ -0,0 +1,12 @@
+﻿namespace WPFReflector
+{
+    internal class Stater
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            App app = new App();
+            app.Run();
+        }
+    }
+}
